47b8ca33ffba5f7b418bec59578fbaa86d88905d
Luis Otavio <luis.otavio2425@gmail.com>
2021-02-27 19:35:06 -0300

    Update Server port

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -29,6 +29,6 @@ app.use(
   },
 );
 
-app.listen(3000, () => {
-  console.log('🚀 Server is running on port 3000!');
+app.listen(3333, () => {
+  console.log('🚀 Server is running on port 3333!');
 });
